36290bc347b906255288ffdcd58a27c726d2df26
meysam <mysm.shahbazi@gmail.com>
2022-03-05 13:15:53 +0330

    change

---
+++ cls/build/CMakeFiles/cls.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cls.dir/main.cpp.o"
+  "cls"
+  "cls.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cls.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
